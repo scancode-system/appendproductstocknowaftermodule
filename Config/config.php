f8dce0513c8876c41a1793e6cb7f0f293ba29a83
Your Name <you@example.com>
2019-12-13 18:31:06 -0300

    .

--- Config/config.php
+++ Config/config.php
@@ -1,27 +1,31 @@
 <?php
 
 return [
-    'name' => 'AppendProductStockNowAfter', 
-    'fields' => ['available_now', 'date_delivery_now', 'available_after', 'date_delivery_after'],
+	'name' => 'AppendProductStockNowAfter', 
+	'fields' => ['available_now', 'date_delivery_now', 'available_after', 'date_delivery_after'],
 	'sample' => [
 		[
 			'name' => 'available_now',
 			'observation' => 'Quantidade atual disponibilizada de produtos para geração de pedidos.',
+			'sample_1' => '500',
 			'filled' => false
 		], 
 		[
 			'name' => 'date_delivery_now',
 			'observation' => 'Data atual aproximada em que o produto será entregue.',
+			'sample_1' => '25/12/2020',
 			'filled' => false
 		],
 		[
 			'name' => 'available_after',
 			'observation' => 'Quantidade futura disponibilizada de produtos para geração de pedidos.',
+			'sample_1' => '1000',
 			'filled' => false
 		], 
 		[
 			'name' => 'date_delivery_after',
 			'observation' => 'Data futura aproximada em que o produto será entregue.',
+			'sample_1' => '07/06/2021',
 			'filled' => false
 		]
 	]
