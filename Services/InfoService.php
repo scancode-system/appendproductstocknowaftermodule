04a76d556bb58ed545f82507310c3a57328fccc0
Your Name <you@example.com>
2020-08-05 10:20:12 -0300

    .

--- Services/InfoService.php
+++ Services/InfoService.php
@@ -4,36 +4,39 @@ namespace Modules\AppendProductStockNowAfter\Services;
 
 
 use Modules\Portal\Services\Validation\Data\InfoValidationService;
+use Modules\Portal\Services\Validation\Data\InfoValidationsService;
  
 
 class InfoService extends InfoValidationService
 {
 
 
+
+
 	public function rule($data, $index, $columns)
 	{
 		return [
 			'available_now' => 'integer|min:0',
-			'date_delivery_now' => 'string|date_format:Y-m-d',
+			'date_delivery_now' => 'date',/*'string|date_format:Y-m-d',*/
 			'available_after' => 'integer|min:0',
-			'date_delivery_after' => 'string|date_format:Y-m-d'
+			'date_delivery_after' => 'date'/*'string|date_format:Y-m-d'*/
 		];
 	}
 
 	public function modifiers()
 	{
-		return [
+		/*return [
 			['rule' => ['date_delivery_now' => 'required|date_format:d/m/Y'], 'filter' => 'dateDMY'],
 			['rule' => ['date_delivery_now' => 'required|date_format:j/n/Y'], 'filter' => 'dateDMY'],
 			['rule' => ['date_delivery_after' => 'required|date_format:d/m/Y'], 'filter' => 'dateDMY'],
 			['rule' => ['date_delivery_after' => 'required|date_format:j/n/Y'], 'filter' => 'dateDMY']
-		];
+		];*/
 	}
 
 
 	public function columnsFormat()
 	{
-		return  [];
+		return ['date_delivery_now' => InfoValidationsService::DATE_FORMAT, 'date_delivery_after' => InfoValidationsService::DATE_FORMAT];
 	}
 
 }
